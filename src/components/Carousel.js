a339cfd5d0534caaf4c390784ce2e1852a85d584
ferrary7 <ary7sharma@gmail.com>
2022-12-28 15:44:07 +0530

    Image carousel with hooks�

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,6 +8,28 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [displayImage, setdisplayImage] = useState(0)
+
+    return(
+        <div className="container">
+            <div className="carousel" style={{ backgroundImage: `url(${images[displayImage].img})` }}>
+                <div className="leftButton" onClick={() =>{
+                    displayImage > 0 && setdisplayImage(displayImage - 1)
+                }}>
+                    <ArrowBackIosIcon />
+                </div>
+                <div className="text">
+                    <h1>{images[displayImage].title}</h1>
+                    <h2>{images[displayImage].subtitle}</h2>
+                </div>
+                <div className="rightButton" onClick={() =>{
+                    displayImage < images.length - 1 && setdisplayImage(displayImage + 1) 
+                }}>
+                    <ArrowForwardIosIcon />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Carousel;
